e012c0bc88ca3cb63b296ed1b1e6d2774d8be12e
Suran Warnakulasooriya <68828123+suranwarnakulasooriya@users.noreply.github.com>
2022-08-29 13:51:07 -0400

    Add files via upload

--- Game_of_Life.py
+++ Game_of_Life.py
@@ -31,57 +31,66 @@
 # ============================================================================================================
 # NEIGHBORS
 # each function returns the neighbor of a specified cell in a certain direction
+# the 'get' specifier tells the function to return the coordinates of the neighbor instead of its value
 
 # find north neighbor
-def find_N(grid,r,c):
+def find_N(grid,r,c,get=False):
     R = r-1
     if r == 0: R = h-1
+    if get: return (R,c)
     return grid[R][c]
 
 # find south neighbor
-def find_S(grid,r,c):
+def find_S(grid,r,c,get=False):
     R = r+1
     if r == h-1: R = 0
+    if get: return (R,c)
     return grid[R][c]
 
 # find east neighbor
-def find_E(grid,r,c):
+def find_E(grid,r,c,get=False):
     C = c+1
     if c == w-1: C = 0
+    if get: return (r,C)
     return grid[r][C]
 
 # find west neighbor
-def find_W(grid,r,c):
+def find_W(grid,r,c,get=False):
     C = c-1
     if c == 0: C = w-1
+    if get: return (r,C)
     return grid[r][C]
 
 # find northeast neighbor
-def find_NE(grid,r,c):
+def find_NE(grid,r,c,get=False):
     R = r-1; C = c+1
     if r == 0: R = h-1
     if c == w-1: C = 0
+    if get: return (R,C)
     return grid[R][C]
 
 # find northwest neighbor
-def find_NW(grid,r,c):
+def find_NW(grid,r,c,get=False):
     R = r-1; C = c-1
     if r == 0: R = h-1
     if c == 0: C = w-1
+    if get: return (r,c)
     return grid[R][C]
 
 # find southeast neighbor
-def find_SE(grid,r,c):
+def find_SE(grid,r,c,get=False):
     R = r+1; C = c+1
     if r == h-1: R = 0
     if c == w-1: C = 0
+    if get: return (R,C)
     return grid[R][C]
 
 # find southwest neighbor
-def find_SW(grid,r,c):
+def find_SW(grid,r,c,get=False):
     R = r+1; C = c-1
     if r == h-1: R = 0
     if c == 0: C = w-1
+    if get: return (R,C)
     return grid[R][C]
 
 # ============================================================================================================
@@ -91,54 +100,59 @@ def find_SW(grid,r,c):
 
 # return number of live neighbors of a cell
 def live_neighbors(grid,pos):
-    r,c = pos
     count = 0
-    for neighbor in neighbors: count += neighbor(grid,r,c)
+    for neighbor in neighbors: count += neighbor(grid,pos[0],pos[1])
     return count
+
+# return list of neighbors
+def get_neighbors(grid,pos):
+    l = [pos]
+    for neighbor in neighbors: l.append(neighbor(grid,pos[0],pos[1],get=True))
+    return l
 
 # return an empty wxh grid
 def empty_grid(w,h):
     return [[0]*w for _ in range(h)]
 
-# update grid to next generation
-def next_gen(grid):
+# update grid to next generation using neighbor search
+def optimize(grid,lc):
     newgrid = empty_grid(w,h) # next generation of grid starts empty (all dead)
-    for r in range(h):     # go through every row and column, effectively covering every cell in the grid
-        for c in range(w):
-            L = live_neighbors(grid,(r,c)) # L is the number of live neighbors
-            if grid[r][c] == 0: # if the current cell is dead...
-                if L == 3: newgrid[r][c] = 1 # come to life if condition met
-            elif grid[r][c] == 1: # if the current cell is alive...
-                if 2 <= L <= 3: newgrid[r][c] = 1 # stay alive if condition met
-    return newgrid
+    C = [] # list of cells to check is the live cells and their neighbors
+    for r,c in lc: C += get_neighbors(grid,(r,c))
+    C = list(set(C)) # remove repeats
+    l = [] # new list of live neighbors
+    for r,c in C:
+        N = live_neighbors(grid,(r,c)) # L is the number of live neighbors
+        if grid[r][c] == 0: # if the current cell is dead...
+            if N == 3: # come to life in condition met
+                newgrid[r][c] = 1; l.append((r,c))
+        elif grid[r][c] == 1: # if the current cell is alive...
+            if 2 <= N <= 3: # stay alive if condidion met
+                newgrid[r][c] = 1; l.append((r,c))
+    return newgrid, l
 
 # draw grid on screen
 def draw_grid(grid,w,h,p):
     for r in range(h):
         for c in range(w):
-            if evolve: pygame.draw.rect(screen, cols_evolve[grid[r][c]], (c*p,r*p,p,p))
-            else: pygame.draw.rect(screen, cols_editor[grid[r][c]], (c*p,r*p,p,p))
+            if evolve and grid[r][c] == 1: pygame.draw.rect(screen, ('#c678dd'), (c*p,r*p,p,p))
+            elif grid[r][c] == 1: pygame.draw.rect(screen, ('#e5c07b'), (c*p,r*p,p,p))
 
 # draw gridlines if permitted
-def draw_lines(w,h,permit):
-    if permit:
-        for i in range(h):
-            pygame.draw.line(screen, linecol, (0, i*p), (sw, i*p))
-            for j in range(w):
-                pygame.draw.line(screen, linecol, (j*p, 0), (j*p, sh))
+def draw_lines(w,h):
+    for i in range(h):
+        pygame.draw.line(screen, '#1e222a', (0, i*p), (sw, i*p))
+        for j in range(w):
+            pygame.draw.line(screen, '#1e222a', (j*p, 0), (j*p, sh))
 
 # ============================================================================================================
 
 # ============================================================================================================
 # CONFIGURABLE VARIABLES
 
-p = 35 # cell size in pixels
-w = 50 # width of grid in cells
-h = 50 # height of grid in cells
-cols_editor = {0:(20,20,20), 1:(247, 208, 32)} # colors that represent the live and dead cells in the simulation
-cols_evolve = {0:(20,20,20), 1:(208, 32, 247)} # same but when in evolve mode
-linecol = (40,50,50)
-gridlines = True # whether gridlines appear at the start or not
+p = 20 # cell size in pixels
+w = 100; h = 100 # width and height of grid in cells
+gridlines = False # whether gridlines appear at the start or not, gridlines affect framerate considerably
 
 # ============================================================================================================
 
@@ -146,65 +160,54 @@ gridlines = True # whether gridlines appear at the start or not
 # SETUP
 
 import pygame
-sw = w*p # width of grid in pixels
-sh = h*p # height of grid in pixels
-neighbors = [find_N, find_S, find_E, find_W, find_NE, find_NW, find_SE, find_SW] # list of all neighbor functions
+sw = w*p; sh = h*p # with and height of screen in pixels
+neighbors = [find_N, find_S, find_E, find_W, find_NE, find_NW, find_SE, find_SW] # list all neighbor functions
 grid = empty_grid(w,h) # initialize grid
-mode = 'editor'
-evolve = False
-user_grid = grid
-mousedown = False
-generation = 0
-
-pygame.init()
-screen = pygame.display.set_mode((sw,sh))
+mode = 'Editor'; evolve = False; generation = 0; run = True
+live_cells = [] # starts with no live cells
+pygame.init(); screen = pygame.display.set_mode((sw,sh))
 
 # ============================================================================================================
 
 # ============================================================================================================
 # EVENT LOOP
 
-while True:
-
-    pygame.display.set_caption(f"Conway's Game of Life - {mode}")
-
+while run:
     if evolve: mode = f'Generation {generation}'
     else: mode = 'Editor'
 
-    if evolve: pygame.time.delay(40) # delays by speed
-
+    if evolve: pygame.time.delay(40) # delay so movement is readable
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: pygame.quit(); exit()
+        if event.type == pygame.QUIT: run = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_ESCAPE]: pygame.quit(); exit()
+    if keys[pygame.K_ESCAPE]: run = False
     if keys[pygame.K_l]: pygame.time.delay(30); gridlines = not gridlines 
 
+    screen.fill('#282c34')
     draw_grid(grid,w,h,p)
-    draw_lines(w,h,gridlines)
-
+    if gridlines: draw_lines(w,h)
     pygame.display.update()
 
     if evolve:
-        grid = next_gen(grid); generation += 1
-        if keys[pygame.K_SPACE]:
-            pygame.time.delay(30)
-            evolve = False
-            grid = empty_grid(w,h)
-            generation = 0
+        grid, live_cells = optimize(grid,live_cells); generation += 1
+        if keys[pygame.K_SPACE]: # reset data
+            pygame.time.delay(60); evolve = False; grid = empty_grid(w,h); generation = 0; live_cells = []
 
     else:
         if keys[pygame.K_SPACE]:
-            pygame.time.delay(30)
-            evolve = True
-        if pygame.mouse.get_pressed()[0]:
-            pygame.time.delay(60)
+            pygame.time.delay(60); evolve = True
+        if pygame.mouse.get_pressed()[0]: # toggle cell on click
+            pygame.time.delay(80)
             pos = pygame.mouse.get_pos()
-            pr = pos[0]; pc = pos[1]
-            for r in range(h):
-                for c in range(w):
-                    if p*c < pr < p*c+p and p*r < pc < p*r+p:
-                        grid[r][c] ^= 1 # bitwise xor with 1 to toggle between 1 and 0
-                        user_grid = grid; break
+            r, c = pos[1]//p, pos[0]//p
+            grid[r][c] ^= 1 # bitwise xor with 1 to toggle between 1 and 0
+            # update list of live cells
+            if grid[r][c] == 1: live_cells.append((r,c))
+            else: live_cells.remove((r,c))
+
+    pygame.display.set_caption(f"Conway's Game of Life - {mode}")
+
+pygame.quit; exit()
 
 # ============================================================================================================
